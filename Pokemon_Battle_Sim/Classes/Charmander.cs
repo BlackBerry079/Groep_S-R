da1c718d2675b0ca3d7ff2ec4eda8854901abc54
Seantheman01 <sean.v.g@outlook.com>
2024-03-13 13:35:51 +0100

    battle sim

---
+++ Pokemon_Battle_Sim/Classes/Charmander.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Pokemon_Battle_Sim
+{
+    internal class Charmander
+    {
+        public string name;
+        public string strength;
+        public string weakness;
+
+        public Charmander(string name, string strength, string weakness)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.weakness = weakness;
+        }
+
+        public string Growl()
+        {
+            return "Charmander!!!";
+        }
+    }
+}
